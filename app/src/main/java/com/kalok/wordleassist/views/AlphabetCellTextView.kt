6358460d2506a9ed759d2a328754925d96bfe55c
Kalok Cheung <kalokcheung14@gmail.com>
2022-03-30 20:15:56 +0800

    Feat: Updated AlphabetCellTextView to align text by gravity to align it to center, updated the background color and text size

--- app/src/main/java/com/kalok/wordleassist/views/AlphabetCellTextView.kt
+++ app/src/main/java/com/kalok/wordleassist/views/AlphabetCellTextView.kt
@@ -1,9 +1,11 @@
 package com.kalok.wordleassist.views
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.setPadding
 
@@ -19,11 +21,11 @@ class AlphabetCellTextView @JvmOverloads constructor(
 
     private fun applyTextStyle() {
         // Set up the style of an alphabet box
-        textAlignment = TEXT_ALIGNMENT_CENTER
+        gravity = Gravity.CENTER
         setTextColor(Color.WHITE)
-        setBackgroundColor(Color.GRAY)
+        setBackgroundColor(Color.parseColor("#818384"))
         typeface = Typeface.DEFAULT_BOLD
-        textSize = 40F
+        textSize = 30F
         setPadding(10)
         // Text for preview
         text = PLACE_HOLDER
