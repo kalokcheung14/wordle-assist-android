85c86620ee076d7d4c4aebbce9399390a3858423
Kalok Cheung <kalok.cheung@101medialab.com>
2023-12-25 16:02:36 +0800

    feat: Make GuessRule dependency abstract

--- app/src/main/java/com/kalok/wordleassist/utilities/GuessRule.kt
+++ app/src/main/java/com/kalok/wordleassist/utilities/GuessRule.kt
@@ -1,13 +1,62 @@
 package com.kalok.wordleassist.utilities
 
-interface GuessRule {
-    fun addMismatchAlphabet(character: Char)
+abstract class GuessRule(dictionaryDataSource: DictionaryDataSource) {
+    protected val vocabList by lazy {
+        dictionaryDataSource.getDictionary()
+    }
 
-    fun addMatchAlphabet(position: Int, alphabet: Char)
+    // Convert string that contains a-z alphabets to a set
+    protected var alphabetList: HashSet<Char> = "abcdefghijklmnopqrstuvwxyz".toHashSet()
+    // Set to store unused alphabets
+    protected var mismatchAlphabetList: HashSet<Char> = HashSet()
+    // Array to store correct alphabets
+    protected var matchedAlphabetList = arrayOfNulls<Char>(Constant.NUM_OF_LETTERS)
+    // List to store misplaced alphabets
+    protected var misplacedAlphabetList: ArrayList<HashSet<Char>> = ArrayList()
 
-    fun addMisplacedAlphabet(position: Int, character: Char)
+    init {
+        initList()
+    }
 
-    fun showGuessList(): ArrayList<String>
+    private fun initList() {
+        // Initialise alphabets array and set
+        matchedAlphabetList = arrayOfNulls(Constant.NUM_OF_LETTERS)
+        repeat(Constant.NUM_OF_LETTERS) {
+            misplacedAlphabetList.add(HashSet())
+        }
+        println("_misplacedAlphabetList ${misplacedAlphabetList.size}")
+    }
 
-    fun clear()
+    fun addMismatchAlphabet(character: Char) {
+        println("addMismatchAlphabet $character")
+        mismatchAlphabetList.add(character)
+    }
+
+    fun addMatchAlphabet(position: Int, alphabet: Char) {
+        println("addMatchAlphabet $alphabet")
+        matchedAlphabetList[position] = alphabet
+    }
+
+    fun addMisplacedAlphabet(position: Int, character: Char) {
+        println("addMisplacedAlphabet $character")
+        val charSet = misplacedAlphabetList[position]
+        charSet.add(character)
+        misplacedAlphabetList[position] = charSet
+    }
+    fun showGuessList(): ArrayList<String> {
+        // Remove unused alphabets from the alphabet list
+        alphabetList.removeAll(mismatchAlphabetList)
+
+        return guess()
+    }
+
+    fun clear() {
+        // Clear all guess criteria
+        mismatchAlphabetList = HashSet()
+        misplacedAlphabetList = ArrayList()
+        matchedAlphabetList = arrayOfNulls(Constant.NUM_OF_LETTERS)
+        initList()
+    }
+
+    protected abstract fun guess(): ArrayList<String>
 }
--- app/src/main/java/com/kalok/wordleassist/utilities/GuessRuleImpl.kt
+++ app/src/main/java/com/kalok/wordleassist/utilities/GuessRuleImpl.kt
@@ -1,57 +1,18 @@
 package com.kalok.wordleassist.utilities
 
-import com.kalok.wordleassist.data.TreeNode
-import com.kalok.wordleassist.data.TreeNodeImpl
-import com.kalok.wordleassist.utilities.Constant.NUM_OF_LETTERS
-
-open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule {
-    private val _vocabList by lazy {
-        dictionaryDataSource.getDictionary()
-    }
-
-    // Convert string that contains a-z alphabets to a set
-    private var _alphabetList: HashSet<Char> = "abcdefghijklmnopqrstuvwxyz".toHashSet()
-    // Set to store unused alphabets
-    private var _mismatchAlphabetList: HashSet<Char> = HashSet()
-    // Array to store correct alphabets
-    private var _matchedAlphabetList = arrayOfNulls<Char>(NUM_OF_LETTERS)
-    // List to store misplaced alphabets
-    private var _misplacedAlphabetList: ArrayList<HashSet<Char>> = ArrayList()
-
-    private val rootNodeMap = hashMapOf<Char, TreeNode<Char>>()
-
-    override fun addMismatchAlphabet(character: Char) {
-        println("addMismatchAlphabet $character")
-        _mismatchAlphabetList.add(character)
-    }
-
-    override fun addMatchAlphabet(position: Int, alphabet: Char) {
-        println("addMatchAlphabet $alphabet")
-        _matchedAlphabetList[position] = alphabet
-    }
-
-    override fun addMisplacedAlphabet(position: Int, character: Char) {
-        println("addMisplacedAlphabet $character")
-        val charSet = _misplacedAlphabetList[position]
-        charSet.add(character)
-        _misplacedAlphabetList[position] = charSet
-    }
-
-    override fun showGuessList(): ArrayList<String> {
+open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule(dictionaryDataSource) {
+    override fun guess(): ArrayList<String> {
         // A list to store matched words
         val matchedWords = ArrayList<String>()
 
-        // Remove unused alphabets from the alphabet list
-        _alphabetList.removeAll(_mismatchAlphabetList)
-
         // Loop through all the possible words
-        for (vocab in _vocabList) {
+        for (vocab in vocabList) {
             // Convert the word to a set
             var vocabCharList = vocab.toHashSet()
 
             // Check if the word contains any unused alphabets
             // If yes, proceed to handle the next word
-            vocabCharList.retainAll(_mismatchAlphabetList)
+            vocabCharList.retainAll(mismatchAlphabetList)
             if (vocabCharList.toTypedArray().isNotEmpty()) {
                 continue
             }
@@ -65,7 +26,7 @@ open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule 
 
                 // If the alphabet at index i is not null
                 // e.g. there is an alphabet input correctly at this index
-                val matchedAlphabet = _matchedAlphabetList[i]
+                val matchedAlphabet = matchedAlphabetList[i]
                 if (matchedAlphabet != null) {
                     // Check if the alphabets match
                     match = letterInVocab == matchedAlphabet
@@ -79,7 +40,7 @@ open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule 
                 // If an alphabet not in this position is in this position
                 // e.g. this word is not a match
                 // so stop checking the rest of the word
-                if (_misplacedAlphabetList[i].contains(letterInVocab)) {
+                if (misplacedAlphabetList[i].contains(letterInVocab)) {
                     match = false
                     break
                 }
@@ -88,7 +49,7 @@ open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule 
                 val allChars = HashSet<Char>()
 
                 // Put all misplaced alphabets in one single set
-                allChars.addAll(_misplacedAlphabetList.flatten())
+                allChars.addAll(misplacedAlphabetList.flatten())
 
                 // Convert the word to a set of alphabets
                 vocabCharList = vocab.toHashSet()
@@ -109,44 +70,5 @@ open class GuessRuleImpl(dictionaryDataSource: DictionaryDataSource): GuessRule 
             }
         }
         return matchedWords
-    }
-
-    override fun clear() {
-        // Clear all guess criteria
-        _mismatchAlphabetList = HashSet()
-        _misplacedAlphabetList = ArrayList()
-        _matchedAlphabetList = arrayOfNulls(NUM_OF_LETTERS)
-        initList()
-    }
-
-    init {
-//        constructVocabTree()
-        initList()
-    }
-
-    private fun initList() {
-        // Initialise alphabets array and set
-        _matchedAlphabetList = arrayOfNulls(NUM_OF_LETTERS)
-        repeat(NUM_OF_LETTERS) {
-            _misplacedAlphabetList.add(HashSet())
-        }
-        println("_misplacedAlphabetList ${_misplacedAlphabetList.size}")
-    }
-
-    private fun constructVocabTree() {
-        _vocabList.forEach {
-            val firstChar = it.first()
-            val rootNode = if (rootNodeMap.containsKey(firstChar)) {
-                rootNodeMap[firstChar]
-            } else {
-                TreeNodeImpl(firstChar).also { node ->
-                    rootNodeMap[firstChar] = node
-                }
-            }
-            var parentNode = rootNode
-            it.substring(1).forEach { char ->
-                parentNode = parentNode?.addChild(char)
-            }
-        }
     }
 }
